1fb693f76833cc84e9dbbc1daeb706ee7a57a5ed
sigit-tunggul <sigittt53@gmail.com>
2021-08-18 23:07:40 +0700

    fix bug comparation table

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Preference;
 use App\Models\Kriteria;
+use Illuminate\Support\Facades\DB;
 
 class PreferenceController extends Controller
 {
@@ -12,15 +13,21 @@ class PreferenceController extends Controller
     {
         $no = 1;
         $preference = Preference::all();
-        return view('preference.index',compact('no','preference'));
+        foreach ($preference as $p) {
+            $p['kriteria1'] = DB::table('kriterias')->where('id', $p->kriteria1)->first()->nama_kriteria;
+            $p['kriteria2'] = DB::table('kriterias')->where('id', $p->kriteria2)->first()->nama_kriteria;
+        }
+        return view('preference.index', compact('no', 'preference'));
     }
 
-    public function create(){
+    public function create()
+    {
         $kriterias = Kriteria::all();
         return view('preference.create', compact('kriterias'));
     }
 
-    public function store(){
+    public function store(Request $request)
+    {
         Preference::create([
             'kriteria1' => request('kriteria1'),
             'lable' => request('lable'),
@@ -30,15 +37,17 @@ class PreferenceController extends Controller
         return redirect()->route('preference.index');
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
 
         $preference = Preference::find($id);
         $kriterias = Kriteria::all();
 
-        return view('preference.edit', compact('preference','kriterias'));
+        return view('preference.edit', compact('preference', 'kriterias'));
     }
 
-    public function update($id){
+    public function update($id)
+    {
         $kriterias = Kriteria::all();
         $preference = Preference::find($id);
         $preference->update([
@@ -50,7 +59,8 @@ class PreferenceController extends Controller
         return redirect()->route('preference.index');
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         $preference = Preference::find($id);
         $preference->delete();
         return redirect()->route('preference.index');
--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -8,61 +8,71 @@ use App\Models\TabelPilihan;
 use App\Models\Prodi;
 use App\Models\Kriteria;
 use App\Models\Preference;
+
 class ResultController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $jurusanSekolah = JurusanSekolah::all();
         return view('welcome', compact('jurusanSekolah'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         session(['name' => $request->name]);
         return ($request->jurusansekolah_id)
-                ? redirect()->route('select')->with(['id' => $request->jurusansekolah_id])
-                : redirect()->route('index');
+            ? redirect()->route('select')->with(['id' => $request->jurusansekolah_id])
+            : redirect()->route('index');
     }
 
-    public function select(Request $request){
-        if($request->session()->get('id')){
+    public function select(Request $request)
+    {
+        if ($request->session()->get('id')) {
             $id = $request->session()->get('id');
             $sekolah = JurusanSekolah::whereId($id)->first();
             $available = TabelPilihan::whereIdJurusanSekolah($id)->get();
-            
+
             return view('result.select', compact('available', 'sekolah'));
-        }else{
+        } else {
             return redirect()->route('index');
         }
     }
 
-    public function storeSelect(Request $request){
+    public function storeSelect(Request $request)
+    {
         return ($request->id_prodi)
             ? redirect()->route('criteria')->with('prodi_id', $request->id_prodi)
             : redirect()->route('index');;
     }
 
-    public function criteria(Request $request){
-        if($request->session()->get('prodi_id')){
+    public function criteria(Request $request)
+    {
+        if ($request->session()->get('prodi_id')) {
             $idProdi = $request->session()->get('prodi_id');
-            $prodi = Prodi::whereIn('id',$idProdi)->get();
+            $prodi = Prodi::whereIn('id', $idProdi)->get();
             $kriteria = Kriteria::all();
             return view('result.criteria', compact('prodi', 'kriteria'));
-        }else{
+        } else {
             return redirect()->route('index');
         }
     }
 
-    public function storeCriteria(Request $request){
+    public function storeCriteria(Request $request)
+    {
         return ($request->data)
             ? redirect()->route('result')->with('data', $request->data)
             : redirect()->route('index');
     }
 
-    public function result(Request $request){
-        if(!$request->session()->get('data')){
+    public function result(Request $request)
+    {
+        if (!$request->session()->get('data')) {
             return redirect()->route('index');
         }
         $kriteria = Kriteria::all();
-        $kriteria[0]->bobot=1;
+        $preference = Preference::all();
+
+        $kriteria[0]->bobot = 1;
         $i = 0;
         $nama = array();
         foreach ($kriteria as $item) {
@@ -72,17 +82,48 @@ class ResultController extends Controller
 
         //get value Pairwise Comparisons
         $comparation = array();
-        for ($i = 0; $i < $kriteria->count(); $i++){
-            for($j = 0; $j < $kriteria->count(); $j++){
-                if($i == 0){
-                    $comparation[$i][$j] = $kriteria[$j]->bobot; 
-                }else{
-                    if($i == $j){
-                        $comparation[$i][$j] = 1; 
-                    }elseif($i > $j){
-                        $comparation[$i][$j] = 1/$comparation[$j][$i];
-                    }else{
-                        $comparation[$i][$j] = $comparation[$i-1][$j-1];
+        for ($i = 0; $i < $kriteria->count(); $i++) { //looping 5x
+            for ($j = 0; $j < $kriteria->count(); $j++) { // setiap looping akan melooping 5x
+                if ($i == 0) { // cek index = 0 (index pertama)
+                    $comparation[$i][$j] = $kriteria[$j]->bobot;
+                    //comparasi array diisi, $comparation[0][0] = 6 (minat:6)
+                    if ($j > $i) {
+                        foreach ($preference as $value) {
+                            //i=0 j=0, looping sebanyak total preferensi
+                            //i=0 j=1, looping sebanyak total preferensi
+                            //i=0 j=2, looping sebanyak total preferensi
+                            if ($kriteria[$i]->id == $value->kriteria1 && $kriteria[$j]->id == $value->kriteria2) {
+                                $comparation[$i][$j] = $value->bobot;
+                            }
+                        }
+                    }
+                    //comparasi array diisi, $comparation[0][1] = 5 (bakat:5)
+                    //comparasi array diisi, $comparation[0][2] = 3 (minat:3)
+                    //comparasi array diisi, $comparation[0][3] = 8 (minat:8)
+                    //comparasi array diisi, $comparation[0][4] = 8 (minat:8)
+                } else {
+                    if ($i == $j) {
+                        $comparation[$i][$j] = 1;   // i==j; 
+                        //$comparation[1][1] = 1
+                        //$comparation[2][2] = 1
+                        //$comparation[3][3] = 1
+                        //$comparation[4][4] = 1
+                        //$comparation[5][5] = 1
+                    } elseif ($i > $j) {
+                        $comparation[$i][$j] = 1 / $comparation[$j][$i];
+                        //$comparation[1][0] = segitiga kiri bawah
+                    } else {
+                        if ($j > $i) {
+                            foreach ($preference as $value) {
+                                if ($kriteria[$i]->id == $value->kriteria1 && $kriteria[$j]->id == $value->kriteria2) {
+                                    $comparation[$i][$j] = $value->bobot;
+                                }
+                            }
+                        }
+                        // $comparation[$i][$j] = $comparation[$i - 1][$j - 1]; // segitiga kanan atas
+                        //$comparation[1][2] = $comparation[$i - 1][$j - 1]; == $comparation[0][1]
+                        //$comparation[1][3] = segitiga kanan atas
+                        //$comparation[1][4] = segitiga kanan atas
                     }
                 }
             }
@@ -90,9 +131,9 @@ class ResultController extends Controller
         $index = 0;
         $jumlahComparation = array();
 
-        while($index < $kriteria->count()){
+        while ($index < $kriteria->count()) {
             $tmp = 0;
-            for($i = 0; $i< $kriteria->count(); $i++){
+            for ($i = 0; $i < $kriteria->count(); $i++) {
                 $tmp += $comparation[$i][$index];
             }
             $jumlahComparation[$index] = $tmp;
@@ -104,11 +145,11 @@ class ResultController extends Controller
         $eigenIndex = 0;
         $eigen = array();
 
-        while($eigenIndex < $kriteria->count()){
+        while ($eigenIndex < $kriteria->count()) {
             $rowTotal = 0;
-            for($i = 0; $i < $kriteria->count(); $i++ ){
+            for ($i = 0; $i < $kriteria->count(); $i++) {
                 $total = 0;
-                for($j = 0; $j < $kriteria->count(); $j++ ){
+                for ($j = 0; $j < $kriteria->count(); $j++) {
                     $eigen[$eigenIndex][$i][$j] = $comparation[$eigenIndex][$j] * $comparation[$j][$i];
                     $total += $eigen[$eigenIndex][$i][$j];
                 }
@@ -122,8 +163,8 @@ class ResultController extends Controller
         //Pencarian EVN : Eigen Vektor Normalisasi
         $evnEigen = array();
         $totalEvnEigen = 0;
-        for($i = 0; $i < $kriteria->count(); $i++){
-            for ($j=0; $j < $kriteria->count(); $j++) { 
+        for ($i = 0; $i < $kriteria->count(); $i++) {
+            for ($j = 0; $j < $kriteria->count(); $j++) {
                 $evnEigen[$i][$j] = $eigen[$i][$j]['total'];
             }
             $evnEigen[$i]['total'] = $eigen[$i]['total'];
@@ -132,24 +173,24 @@ class ResultController extends Controller
 
         $evnEigen['total'] = $totalEvnEigen;
 
-        for($i = 0; $i < $kriteria->count(); $i++){
-            for ($j=0; $j < $kriteria->count(); $j++) { 
+        for ($i = 0; $i < $kriteria->count(); $i++) {
+            for ($j = 0; $j < $kriteria->count(); $j++) {
                 $evnEigen[$i]['evn'] = $eigen[$i]['total'] / $evnEigen['total'];
             }
         }
         //pecarian rasio konsistensi
         $emax = 0;
-        for ($i=0; $i < $kriteria->count(); $i++) { 
+        for ($i = 0; $i < $kriteria->count(); $i++) {
             $emax += $comparation['total'][$i] * $evnEigen[$i]['evn'];
         }
-        $cl = ($emax - $kriteria->count())/($kriteria->count()-1);
+        $cl = ($emax - $kriteria->count()) / ($kriteria->count() - 1);
         $cr = $cl / 0.58;
         $rasio = [
             'emaks' => $emax,
             'cl' => $cl,
             'cr' => $cr
         ];
-        
+
         //result
         $ahp = [
             'nama' => $nama,
@@ -158,14 +199,15 @@ class ResultController extends Controller
             'evn_eigen' => $evnEigen,
             'rasio' => $rasio
         ];
+        // dd($ahp);
 
         //SAW metoda
         $data = $request->session()->get('data');
         $maxArr = array();
-        for ($i=0; $i < (count($data[0])-1); $i++) { 
+        for ($i = 0; $i < (count($data[0]) - 1); $i++) {
             $max = 0;
-            for ($j=0; $j < count($data); $j++) { 
-                if($data[$j][$i] > $max){
+            for ($j = 0; $j < count($data); $j++) {
+                if ($data[$j][$i] > $max) {
                     $max = $data[$j][$i];
                 }
             }
@@ -173,16 +215,16 @@ class ResultController extends Controller
         }
 
         $next = array();
-        for ($i=0; $i < count($data); $i++) { 
-            for ($j=0; $j < count($ahp['nama']); $j++) { 
+        for ($i = 0; $i < count($data); $i++) {
+            for ($j = 0; $j < count($ahp['nama']); $j++) {
                 $next[$i][$j] = $data[$i][$j] / $maxArr[$j];
             }
         }
 
         $final = array();
-        for ($i=0; $i < count($data); $i++) { 
+        for ($i = 0; $i < count($data); $i++) {
             $tmp = 0;
-            for ($j=0; $j < count($ahp['nama']); $j++) { 
+            for ($j = 0; $j < count($ahp['nama']); $j++) {
                 $final[$i][$j] = $next[$i][$j] * $ahp['evn_eigen'][$j]['evn'];
                 $tmp += $final[$i][$j];
             }
@@ -191,7 +233,8 @@ class ResultController extends Controller
         }
 
         //mencari hasil yang paling tinggi
-        usort($final, function($first, $second){
+        usort($final, function ($first, $second) {
+            // return strlen($first['total']) <=> strlen($second['total']);
             return $first['total'] < $second['total'];
         });
 
@@ -202,7 +245,6 @@ class ResultController extends Controller
             'final' => $final,
             'name' => $request->session()->get('name')
         ];
-
         return view('result.result', compact('ahp', 'saw'));
     }
 }
